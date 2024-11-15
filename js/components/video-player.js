75f2413cb04b8ec911334dedde8ede2d6bb9f126
Jérôme Coupé <jercoupe@gmail.com>
2024-11-15 09:11:40 +0100

    code cleanup

--- js/components/video-player.js
+++ js/components/video-player.js
@@ -1,73 +1,72 @@
 /**
- * A minimal <video-player></video-player> component
+ * A minimal <video-player> component
  *
  * - works for Youtube and Vimeo. Other video services using an iframe can be added.
  * - progressively enhances a link wrapped around an image into an iframe
  */
 class videoPlayer extends HTMLElement {
   constructor() {
-    // call super
     super();
-
-    // variables
-    this.provider = this.dataset.videoProvider.toLowerCase();
-    this.videoId = this.dataset.videoId;
-    this.autoplay = this.hasAttribute("data-autoplay") ? 1 : 0;
-    this.videoLink = this.querySelector("a");
   }
 
   /**
-   * Build iframe url based on provider and id
-   * @param {string} provider - "youtube" or "vimeo"
-   * @param {string} id - id of the video
-   * @returns {string} full url for the iframe
+   *
+   * @param {string} id - ID of the video
+   * @param {string} provider - name of the provider
+   * @param {number} autoplay - 1 or 0
+   * @returns {object} - iframe element with attributes
    */
-  buildIframeSrc(provider, id, autoplay) {
-    let fullUrl = "";
+  buildIframe(id, provider, autoplay) {
+    let iframeSrc = "";
 
+    // youtube
     if (provider === "youtube") {
-      fullUrl = new URL(`https://www.youtube-nocookie.com/embed/${id}`);
-      fullUrl.searchParams.set("autoplay", autoplay);
+      iframeSrc = new URL(`https://www.youtube-nocookie.com/embed/${id}`);
+      iframeSrc.searchParams.set("autoplay", autoplay);
     }
 
+    // vimeo
     if (provider === "vimeo") {
-      fullUrl = new URL(`https://player.vimeo.com/video/${id}`);
-      fullUrl.searchParams.set("dnt", true);
-      fullUrl.searchParams.set("autoplay", autoplay);
+      iframeSrc = new URL(`https://player.vimeo.com/video/${id}`);
+      iframeSrc.searchParams.set("dnt", true);
+      iframeSrc.searchParams.set("autoplay", autoplay);
     }
 
-    return fullUrl;
-  }
-
-  /**
-   * Initiated when component is loaded in the DOM
-   * @returns void
-   */
-  connectedCallback() {
-    // check if we have a link, video ID, video provider
-    if (!this.videoLink || !this.provider || !this.videoId) return;
-
-    // prepare iframe src
-    const iframeSrc = this.buildIframeSrc(
-      this.provider,
-      this.videoId,
-      this.autoplay
-    );
-
-    // bail out if no src
-    if (!iframeSrc) return;
-
-    // prepare iframe
     const iframeEl = document.createElement("iframe");
     iframeEl.src = iframeSrc;
     iframeEl.allowFullscreen = true;
     iframeEl.width = 1500;
     iframeEl.height = 844;
 
+    return iframeEl;
+  }
+
+  /**
+   * Initiated when component is loaded in the DOM
+   *
+   * @returns void
+   */
+  connectedCallback() {
+    // config
+    const allowedProviders = ["youtube", "vimeo"];
+
+    // params
+    const provider = this.dataset.videoProvider.toLowerCase();
+    const videoId = this.dataset.videoId;
+    const autoplay = this.hasAttribute("data-autoplay") ? 1 : 0;
+    const videoLink = this.querySelector("a");
+
+    // check if we have a link, video ID
+    if (!videoLink || !videoId) return;
+    if (!allowedProviders.includes(provider)) return;
+
+    // build iframe
+    const iframeEl = this.buildIframe(videoId, provider, autoplay);
+
     // click handler
-    this.videoLink.addEventListener("click", (event) => {
+    videoLink.addEventListener("click", (event) => {
       event.preventDefault();
-      this.videoLink.replaceWith(iframeEl);
+      videoLink.replaceWith(iframeEl);
     });
   }
 }
